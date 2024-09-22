196a73475db1a536df427868cefce5301709689e
Vin-Truevana <164653705+Vin-Truevana@users.noreply.github.com>
2024-09-21 19:56:16 -0700

    autocomplete script

--- autocomplete/index.js
+++ autocomplete/index.js
@@ -1,1 +1,25 @@
-//test
+window.fsAttributes = window.fsAttributes || [];
+window.fsAttributes.push([
+  'cmsnest',
+  (filterInstances) => {
+    const locations = document.querySelectorAll(
+        '[wb-autocomplete="locations"]'
+    );
+
+    //resolve duplicates
+    const locationSet = new Set();
+    locations.forEach((location) => {
+        locationSet.add(location.textContent);
+    });
+    const uniqueLocationsArray = [...locationSet];
+
+    //create new instance of autocomplete
+    const autoCompleteJS = new autoComplete({
+        selector: "#autoComplete",
+        data: {
+            src: uniqueLocationsArray
+        }
+    });
+    autoCompleteJS.start();
+  },
+]);
